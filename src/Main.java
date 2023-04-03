a7c8163669c6f36312dafd015e271d51320d08f1
Poteryashka163 <scrat0072@gmail.com>
2023-04-04 01:22:49 +0400

    Начало дз, решение задачи 1.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,28 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        task1();
+        Scanner scanner = new Scanner(System.in);
+        task1(scanner);
         task2();
         task3();
     }
 
-    public static void task1 () {
+    public static void task1 (Scanner scanner) {
         System.out.println("Задача 1");
+        System.out.println("Введите Имя- ");
+        String firstName = scanner.next();
+        System.out.println("Введите Фамилмю- ");
+        String lastName = scanner.next();
+        System.out.println("Введите Отчество- ");
+        String middleName = scanner.next();
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника - "+fullName);
+
     }
 
     public static void task2 () {
         System.out.println("Задача 2");
+        
     }
     public static void task3 () {
         System.out.println("Задача 3");
