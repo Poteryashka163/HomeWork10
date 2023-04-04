219442c3330e3462c241f82f8df916a98332a18c
Poteryashka163 <scrat0072@gmail.com>
2023-04-04 23:54:27 +0400

    Дз на проверку

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         task1(scanner);
-        task2();
+        task2(scanner);
         task3();
     }
 
@@ -20,11 +22,26 @@ public class Main {
 
     }
 
-    public static void task2 () {
+    public static void task2 (Scanner scanner) {
         System.out.println("Задача 2");
+        System.out.println("Введите Имя- ");
+        String firstName = scanner.next();
+        System.out.println("Введите Фамилмю- ");
+        String lastName = scanner.next();
+        System.out.println("Введите Отчество- ");
+        String middleName = scanner.next();
+        String fullName = lastName + " " + firstName + " " + middleName;
+        // String fullName1 = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — "+(fullName.toUpperCase()));
         
     }
     public static void task3 () {
         System.out.println("Задача 3");
+        String fullName = "Иванов Семён Семёнович";
+        String newfullName =  fullName.replace('ё', 'е');
+        System.out.println("Данные ФИО сотрудника —"+newfullName);
+
+
+
     }
 }
